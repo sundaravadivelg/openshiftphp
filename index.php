bcaa52b2ddfaf176eef584c9e3fd4ffe72fc1b20
sundaravadivelg <98506937+sundaravadivelg@users.noreply.github.com>
2022-01-31 10:22:06 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP 2022  Test</h1>
-		<p><b> CITI (Sundar's) after webhook Session </b></p>
+		<p><b> CITI (Sundar's) after webhook and after url change Session </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION 2 </h2> 
